79e9428b1c8752baf155b31ef76d3fb45bef09aa
Flavio <flavioluiscarneiro@gmail.com>
2024-04-12 10:37:26 -0300

    [IMP] testa pesquisas no google

--- tests-selenium/src/test/java/app/EdgeDriverTest.java
+++ tests-selenium/src/test/java/app/EdgeDriverTest.java
@@ -1,8 +1,13 @@
 package app;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class EdgeDriverTest {
     private WebDriver driver;
@@ -14,12 +19,16 @@ public class EdgeDriverTest {
         driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get(GOOGLE_LINK);
-        driver.quit();
     }
 
     @Test
-    public void deve_abrir_o_google() {
+    public void deveFazerPesquisaNoGoogle() {
         init();
+        WebElement inputGoogle = driver.findElement(By.id("APjFqb"));
+        inputGoogle.sendKeys("teste" + Keys.ENTER);
+
+        String result = driver.findElement(By.id("result-stats")).getText();
+        assertTrue(result, result.contains("Aproximadamente"));
     }
 
 }
